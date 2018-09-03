ff86de6d656c0b8d97eec88e0b2f821fcdf7206a
Kim JeongHyun <ashcroft147@gmail.com>
2018-09-03 23:31:22 +0900

    Add edge rect

--- src/scripts/graphic/edgeRect.js
+++ src/scripts/graphic/edgeRect.js
@@ -1,5 +1,42 @@
 import * as d3 from 'd3';
-import $ from 'jquery';
 
-// 3. Edge Rect 
-$( "<div id='rect-edge'>3.Edge Rect</div>" ).appendTo( '#graphic' );
+// 3. Edge Rect
+d3.select("#graphic").append("div")
+        .text("3.Edge Rect inside Container Rect");
+
+d3.select("#graphic").append("div")
+        .attr("id", "edge-rect");
+
+var svg = d3.select("#edge-rect").append("svg")
+        .attr("width", 1200)
+        .attr("height", 150);
+
+// Container
+var rect_container = svg.append("rect")
+                            .style('fill', 'yellow')
+                            .attr("x", 10)
+                            .attr("y", 10)
+                            .attr("width", 200)
+                            .attr("height", 100)
+                            .style("stroke","#000000");
+
+var rect_left = svg.append("rect")
+                    .style('fill', 'red')
+                    .attr("x", 10)
+                    .attr("y", 90)
+                    .attr("width", 20)
+                    .attr("height", 20)
+                    .style("stroke","#000000");
+
+var rect_right = svg.append("rect")
+                    .style('fill', 'blue')
+                    .attr("x", 190)
+                    .attr("y", 10)
+                    .attr("width", 20)
+                    .attr("height", 20)
+                    .style("stroke","#000000");
+                           
+svg.append('text').style('fill', 'black')
+                        .attr('x', 70)
+                        .attr('y', 130)
+                        .text('Rect in Rect');
